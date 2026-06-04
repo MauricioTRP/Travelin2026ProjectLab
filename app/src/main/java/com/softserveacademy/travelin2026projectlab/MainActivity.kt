1c764131057c9688c3841af77956ef22e644830e
Mauricio Fuentes <mauriciofb@gmail.com>
2026-06-04 15:51:25 -0400

    First Commit of Project Lab for SoftServe

---
+++ app/src/main/java/com/softserveacademy/travelin2026projectlab/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.softserveacademy.travelin2026projectlab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.softserveacademy.travelin2026projectlab.ui.theme.Travelin2026ProjectLabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Travelin2026ProjectLabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Travelin2026ProjectLabTheme {
+        Greeting("Android")
+    }
+}
